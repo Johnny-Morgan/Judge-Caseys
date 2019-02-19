72f73f39d921bee3457d879e04484607bd80b592
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-02-19 23:42:15 +0000

    Added about-us section & scroll function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     
     
-    
+    // For the sticky navigation
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -10,6 +10,15 @@ $(document).ready(function(){
         }
     },{
         offset: '60px;'
+    });
+    
+    // Scroll on buttons
+    $('.js--scroll-to-about-us').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-about-us').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
 
 });
